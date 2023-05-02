61b167fff714799e600b1d0f840a3d2dbeb9f8d7
Mariam Naser <40898548+mariamnaser@users.noreply.github.com>
2023-05-02 16:45:06 -0400

    comp code

--- tcpclient.py
+++ tcpclient.py
@@ -1,110 +1,138 @@
-"""
-/*******************************************************************************
- * Name          : tcpclient.py
- * Author        : Mariam Naser
- * Date          : April 25, 2023
- * Description   : TCP using UDP base
- ******************************************************************************/
-"""
-##Libraries
-##Libraries
 import socket
-import sys
+import hashlib
+import zlib
+import time
 import logging
 
+logging.basicConfig(level=logging.DEBUG)
+#get machine IP
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
-class sender:
-    def __init__(self, input, address_of_udpl, port_number_of_udpl, windowsize, ack_port_number):
-        logging.basicConfig(filename='udp_log.txt', level=logging.DEBUG)
-        self.input = input
-        self.recv_addr = (address_of_udpl, port_number_of_udpl)
-        self.windowsize = windowsize
-        self.ack_port = ack_port_number
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        hostname=socket.gethostname()
-        IPAddr=socket.gethostbyname(hostname)
-        print("Your Computer Name is:"+hostname)
-        print("Your Computer IP Address is:"+IPAddr)
+def compute_tcp_checksum(data, src_addr, src_port, dst_port):
+    pseudo_header = src_addr + socket.inet_aton(ip_address) + b'\x00' + socket.IPPROTO_TCP.to_bytes(1, byteorder='big') + len(data).to_bytes(2, byteorder='big')
+    tcp_segment = src_port.to_bytes(2, byteorder='big') + dst_port.to_bytes(2, byteorder='big') + b'\x00\x00\x00\x00' + data
+    segment_and_pseudo = pseudo_header + tcp_segment
+    if len(segment_and_pseudo) % 2 != 0:
+        segment_and_pseudo += b'\x00'
+    s = sum(int.from_bytes(segment_and_pseudo[i:i+2], byteorder='big') for i in range(0, len(segment_and_pseudo), 2))
+    s = (s >> 16) + (s & 0xffff)
+    s += s >> 16
+    return ~s & 0xffff
 
-    def start(self):
-        # Establish connection with receiver
-        print(address_of_udpl, int(port_number_of_udpl))
-        if not self.establish_connection(self.remote_addr):
-            logging.error('Could not establish connection')
-            return
+# Function to send a SYN message to the server to initiate the handshake
+def send_syn(sock, server_ip, server_port):
+    logging.debug("Sending SYN to %s:%d", server_ip, server_port)
+    sock.sendto(b"SYN", (server_ip, server_port))
 
-        # Start sending data
-        self.send_data()
+# Function to send an ACK message to complete the handshake and establish the connection
+def send_ack(sock, addr):
+    logging.debug("Sending ACK to %s:%d", addr[0], addr[1])
+    sock.sendto(b"ACK", addr)
 
-    def establish_connection(self, remote_addr):
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print('1')
-        # Set timeout for receiving ACK
-        udp_socket.settimeout(.5)
+# Function to send a packet to the server
+def send_packet(sock, seq_num, packet, remote_addr, remote_port, local_port, window_size):
+    # Set the TCP header fields
+    source_port = local_port # Use the local port passed as argument
+    dest_port = remote_port # Use the remote port passed as argument
+    seq_num = seq_num.to_bytes(4, byteorder='big')
+    ack_num = b'\x00\x00\x00\x00' # Set to zero
+    data_offset_reserved_flags = b'\x50\x00' # Data offset = 5, Reserved = 0, Flags = 0
+    window_size = window_size.to_bytes(2, byteorder='big') # Convert window size to bytes
+    checksum = b'\x00\x00' # Set to zero
+    urgent_pointer = b'\x00\x00' # Set to zero
 
-        # Send SYN packet to remote address
-        syn_packet = b'SYN'
+    # Concatenate the header and packet data
+    tcp_header = source_port + dest_port + seq_num + ack_num + data_offset_reserved_flags + window_size + checksum + urgent_pointer
+    data = tcp_header + packet
+
+    # Compute the TCP checksum over the TCP header and data
+    checksum = compute_tcp_checksum(data, remote_addr, source_port, dest_port)
+
+    # Replace the checksum field in the header with the computed value
+    checksum_bytes = checksum.to_bytes(2, byteorder='big')
+    tcp_header = tcp_header[:16] + checksum_bytes + tcp_header[18:]
+
+    # Send the packet to the remote address and port
+    logging.debug("Sending packet %d to %s:%d", seq_num, remote_addr, remote_port)
+    sock.sendto(data, (remote_addr, remote_port))
+
+# Function to send a FIN message to signal the end of transmission
+
+def send_fin(sock, remote_addr, remote_port):
+    logging.debug("Sending FIN to %s:%d", remote_addr, remote_port)
+    sock.sendto(b"FIN", (remote_addr, remote_port))
+#another thing
+
+def send_file_over_udp(file_path, server_ip, server_port, local_port, window_size, ack_port):
+    # Create a UDP socket and bind it to the local port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', local_port))
+
+    # Set up the sliding window parameters
+    TIMEOUT = 0.5 # Initial timeout in seconds
+    THRESHOLD = 16 # Initial threshold value
+    buffer = []
+
+    # Send a SYN message to the server to initiate the handshake
+    send_syn(sock, server_ip, server_port)
+
+    # Wait for a SYN-ACK message from the server
+    remote_addr = None
+    remote_port = None
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if data == b"SYN-ACK":
+            # Send an ACK message to complete the handshake and establish the connection
+            send_ack(sock, addr)
+            remote_addr = addr[0]
+            remote_port = int(addr[1])
+            logging.info("Connected to remote address: %s remote port: %d", remote_addr, remote_port)
+            break
+
+    # Read the file and split it into packets
+    packet_size = 1024
+    with open(file_path, 'rb') as file:
+        while True:
+            packet = file.read(packet_size)
+            if not packet:
+                break
+            seq_num = len(buffer)
+            checksum, hash_val = compute_checksum_and_hash(packet)
+            buffer.append((seq_num, packet, checksum, hash_val))
+
+    # Send the packets in the buffer using the selective repeat protocol
+    base = 0
+    next_seq_num = 0
+    packets_in_flight = {} # Dictionary to keep track of packets in flight
+    last_ack_time = time.monotonic() # Record the time of the last received ACK
+    while base < len(buffer):
+        # Send packets up to the window size or until the end of the buffer
+        while next_seq_num < base + window_size and next_seq_num < len(buffer):
+            seq_num, packet, checksum, hash_val = buffer[next_seq_num]
+            send_packet(sock, seq_num, packet, checksum, hash_val, remote_addr, remote_port)
+            packets_in_flight[seq_num] = (packet, checksum, hash_val)
+            next_seq_num += 1
+
+        # Check for ACKs from the server
         try:
-            udp_socket.sendto(syn_packet, remote_addr)
-            print('sent')
-            logging.debug(f'Sent SYN packet to {remote_addr}')
-        except Exception as e:
-            logging.error(f'Error sending SYN packet: {str(e)}')
-            return False
-        print('2')
-        # Wait for SYN-ACK packet from remote address
-        try:
-            syn_ack_packet, remote_addr = udp_socket.recvfrom(1024)
-            if syn_ack_packet == b'SYN-ACK':
-                print('got reponse')
-                logging.debug(f'Received SYN-ACK packet from {remote_addr}')
-            else:
-                logging.warning(f'Received unexpected packet from {remote_addr}: {syn_ack_packet}')
-                return False
+            data, addr = sock.recvfrom(1024)
         except socket.timeout:
-            logging.warning(f'Timeout waiting for SYN-ACK packet from {remote_addr}')
-            return False
-        except Exception as e:
-            logging.error(f'Error receiving SYN-ACK packet: {str(e)}')
-            return False
+            # Timeout occurred, retransmit all packets in flight
+            logging.error("Timeout occurred, retransmitting packets")
+            TIMEOUT *= 2
+            THRESHOLD = max(THRESHOLD / 2, 1)
+            for seq_num, packet_info in packets_in_flight.items():
+                packet, checksum, hash_val = packet_info
+                send_packet(sock, seq_num, packet, checksum, hash_val, remote_addr, remote_port)
+            continue
 
-        # Send ACK packet to remote address
-        ack_packet = b'ACK'
-        try:
-            udp_socket.sendto(ack_packet, remote_addr)
-            logging.debug(f'Sent ACK packet to {remote_addr}')
-            print('sent ack')
-            return True
-        except Exception as e:
-            logging.error(f'Error sending ACK packet: {str(e)}')
-            return False
+        if addr[1] != ack_port:
+            # Ignore packets from other ports
+            continue
 
-#tcpclient file address_of_udpl port_number_of_udpl windowsize ack_port_number
-#tcpclient input.txt address_of_udpl port_number_of_udpl windowsize ack_port_number
-
-def main():
-    args = sys.argv[1:]
-    print (len(args))
-
-    input = args[0]
-    address_of_udpl = args[1]
-    port_number_of_udpl = int(args[2])
-    windowsize = int(args[3])
-    ack_port_number = int(args[4])
-    print(address_of_udpl, int(port_number_of_udpl))
-
-    # create a new instance of sender
-    client = sender(input, address_of_udpl, port_number_of_udpl, windowsize, ack_port_number)
-
-    # initiate the handshake
-    remote_addr = (address_of_udpl, ack_port_number)
-    if not client.establish_connection(remote_addr):
-        print('Connection establishment failed')
-        return
-
-    # call the start method to send the data
-    client.start()
-
-
-if __name__ == '__main__':
-    main()
+        if data == b"FIN":
+            # Send a FIN request to signal the end of transmission
+            send_fin(sock, remote_addr, remote_port)
+            print("Sent FIN request to remote address:", remote_addr, "remote port:", remote_port)
+            logging.info("Transmission complete")
--- tcpserver.py
+++ tcpserver.py
@@ -1,111 +1,145 @@
-"""
-/*******************************************************************************
- * Name          : tcpEmulator.py
- * Author        : Mariam Naser
- * Date          : April 25, 2023
- * Description   : TCP using UDP base
- ******************************************************************************/
-"""
-##Libraries
-##Libraries
-import socket
-import sys
-import logging
-import socket
-import sys
+import socketimport socket
+import hashlib
 import logging
 
-class Receiver:
-    def __init__(self, outputfile, listening_port, address_for_acks, port_for_acks):
-        logging.basicConfig(filename='udp_log.txt', level=logging.DEBUG)
-        self.stdout = outputfile
-        self.listening_port = listening_port
-        self.address_for_acks = address_for_acks
-        self.port_for_acks = port_for_acks
-        self.is_listening = False
-        hostname=socket.gethostname()
-        self.IPAddr=socket.gethostbyname(hostname)
-        print("Your Computer Name is:"+hostname)
-        #print("Your Computer IP Address is:"+IPAddr)
-    def listen_for_connections(self):
-        """
-        Listens for incoming connection requests.
-        """
-        print('here')
-        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_socket.bind((self.IPAddr, self.listening_port))
+#get machine IP
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
-        while True:
-            # Wait for SYN packet from remote address
-            try:
-                syn_packet, remote_addr = udp_socket.recvfrom(1024)
-                print(syn_packet)
-                if syn_packet == b'SYN':
-                    print(syn_packet)
-                    logging.debug(f'Received SYN packet from {remote_addr}')
+TIMEOUT = 0.5
+logging.basicConfig(level=logging.ERROR)
 
-                    # Send SYN-ACK packet to remote address
-                    syn_ack_packet = b'SYN-ACK'
-                    print(syn_ack_packet)
-                    udp_socket.sendto(syn_ack_packet, remote_addr)
-                    logging.debug(f'Sent SYN-ACK packet to {remote_addr}')
+def receive_packet(sock):
+    """
+    Receive a packet from the socket and return the sequence number, packet, checksum, and hash.
+    """
+    packet_data, addr = sock.recvfrom(1024)
 
-                    # Wait for ACK packet from remote address
-                    ack_packet, remote_addr = udp_socket.recvfrom(1024)
-                    if ack_packet == b'ACK':
-                        logging.debug(f'Received ACK packet from {remote_addr}')
+    # Parse the TCP header fields
+    source_port = int.from_bytes(packet_data[:2], byteorder='big')
+    dest_port = int.from_bytes(packet_data[2:4], byteorder='big')
+    seq_num = int.from_bytes(packet_data[4:8], byteorder='big')
+    ack_num = int.from_bytes(packet_data[8:12], byteorder='big')
+    data_offset_reserved_flags = int.from_bytes(packet_data[12:14], byteorder='big')
+    window_size = int.from_bytes(packet_data[14:16], byteorder='big')
+    checksum = int.from_bytes(packet_data[16:18], byteorder='big')
+    urgent_pointer = int.from_bytes(packet_data[18:20], byteorder='big')
 
-                        # Start data transfer
-                        self.transfer_data(udp_socket, remote_addr)
+    # Extract the packet data
+    packet = packet_data[20:].decode()
 
-                    else:
-                        logging.warning(f'Received unexpected packet from {remote_addr}: {ack_packet}')
+    # Compute the TCP checksum over the TCP header and data
+    tcp_header = packet_data[:20]
+    computed_checksum = compute_tcp_checksum(tcp_header + packet.encode(), addr[0], dest_port, source_port)
 
-                else:
-                    logging.warning(f'Received unexpected packet from {remote_addr}: {syn_packet}')
+    # Verify the checksum
+    if computed_checksum != checksum:
+        logging.warning("Checksum mismatch: computed=%d, received=%d", computed_checksum, checksum)
 
-            except socket.timeout:
-                logging.warning(f'Timeout waiting for SYN packet')
-            except Exception as e:
-                logging.error(f'Error during connection setup: {str(e)}')
+    # Return the parsed fields and packet data
+    return seq_num, packet, checksum, computed_checksum
 
-    def transfer_data(self, udp_socket, remote_addr):
-        """
-        Transfers data between the remote address and the local client.
-        """
-        # TODO: Implement data transfer
-        pass
+def send_nak(seq_num, sock, addr):
+    """
+    Send a NAK message for the packet with the given sequence number to the specified address.
+    """
+    sock.sendto(str(seq_num).encode(), addr)
+    logging.debug("Sent NAK for packet with sequence number: %s", seq_num)
 
-    def start(self):
-        """
-        Starts the receiver to listen for incoming connections.
-        """
-        self.is_listening = True
-        print('initiated')
-        self.listen_for_connections()
+def selective_repeat_receiver(outputfile, listening_port, address_for_acks, port_for_acks):
+    """
+    Receive packets using the selective repeat protocol with checksum and hash verification.
+    """
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(('0.0.0.0', listening_port))
 
-    def stop(self):
-        """
-        Stops the receiver from listening for incoming connections.
-        """
-        self.is_listening = False
+        # Set up the sliding window parameters
+        WINDOW_SIZE = 10
+        buffer = []
 
-#tcpserver file listening_port address_for_acks port_for_acks
-#tcpserver output.txt 5050 127.0.0.1 5055
-def main():
-    args = sys.argv[1:]
-    print (len(args))
-    print(args)
-    outputfile = args[0]
-    listening_port = int (args[1])
-    address_for_acks = args[2]
-    port_for_acks = int (args[3])
+        # Wait for a SYN message from the sender to initiate the handshake
+        data, addr = sock.recvfrom(1024)
+        if data == b"SYN":
+            # Send a SYN-ACK message to the sender
+            sock.sendto(b"SYN-ACK", addr)
 
-    # Create the TCP client
-    client = Receiver(outputfile, listening_port, address_for_acks, port_for_acks)
+            # Wait for an ACK message from the sender to complete the handshake and establish the connection
+            data, addr = sock.recvfrom(1024)
+            if data == b"ACK":
+                remote_addr = addr[0]
+                remote_port = int(addr[1])
+                logging.info("Connected to remote address: %s remote port: %s", remote_addr, remote_port)
 
-    # Start the TCP client
-    client.start()
+                expected_seq_num = 0
+                received_packets = {} # Dictionary to keep track of received packets
+                timers = {} # Dictionary to keep track of timers for each packet
+                while True:
+                    # Check if any timers have expired
+                    for seq_num, timer in timers.items():
+                        if time.time() > timer:
+                            # Timer has expired, packet is lost, retransmit it
+                            packet_data, addr = received_packets[seq_num]
+                            sock.sendto(packet_data, addr)
+                            timers[seq_num] = time.time() + TIMEOUT
 
-if __name__ == '__main__':
-    main()
+                    sock.settimeout(TIMEOUT)
+                    try:
+                        seq_num, packet, packet_checksum, packet_hash = receive_packet(sock)
+                        logging.debug("Received packet with sequence number: %s", seq_num)
+
+                        # Verify the checksum and hash
+                        if hashlib.sha256(packet.encode()).hexdigest() == packet_checksum and hashlib.md5(packet.encode()).hexdigest() == packet_hash:
+                            if seq_num == expected_seq_num:
+                                received_packets[seq_num] = packet
+                                timers[seq_num] = time.time() + TIMEOUT
+                                while expected_seq_num in received_packets:
+                                    expected_seq_num += 1
+                                # write the packet to the output file
+                                with open(outputfile, 'a') as file:
+                                    file.write(packet)
+                                # Check if any packets in the buffer can now be sent
+                                for seq_num, packet_data, addr in buffer:
+                                    if seq_num in received_packets:
+                                        buffer.remove((seq_num, packet_data, addr))
+                                        # Send the packet to the receiver
+                                        sock.sendto(packet_data, addr)
+                                        timers[seq_num] = time.time() + TIMEOUT
+                            else:
+                                # Packet is out of sequence
+                                if seq_num not in received_packets:
+                                    received_packets[seq_num] = packet
+                                    send_nak(seq_num, sock, addr)
+                                    # Add packet to buffer for potential retransmission
+                                    buffer.append((seq_num, packet_data, addr))
+                                    timers[seq_num] = time.time() + TIMEOUT
+                        else:
+                            # Packet is corrupted
+                            send_nak(seq_num, sock, addr)
+                            # Add packet to buffer for potential retransmission
+                            buffer.append((seq_num, packet_data, addr))
+                            timers[seq_num] = time.time() + TIMEOUT
+                        # Perform congestion control to reduce packet loss
+                        if len(buffer) > WINDOW_SIZE:
+                            # Send a NAK for all packets in the buffer
+                            for seq_num, packet_data, addr in buffer:
+                                send_nak(seq_num, sock, addr)
+                            buffer = []
+
+                    except socket.timeout:
+                        logging.info("Timeout occurred while waiting for packet")
+                        # Send a NAK for all expected packets in the window
+                        for seq_num in range(expected_seq_num, expected_seq_num + WINDOW_SIZE):
+                            if seq_num not in received_packets:
+                                send_nak(seq_num, sock, addr)
+                            # Restart timer for the first unacknowledged packet
+                                timer = Timer(TIMEOUT_DURATION, handle_timeout, args=[expected_seq_num])
+                                timer.start()
+                        # Clear the buffer since all packets in it should have been retransmitted by now
+                        buffer = []
+
+
+        except socket.timeout:
+            logging.info("Timeout occurred while waiting for packet")
+            # Send a NAK for all expected packets in the window
+            for seq_num in range(expected_seq_num, expected_seq_num + WINDOW_SIZE
